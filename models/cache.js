8381bc6873a34789d8ccc224f52f2974923e4cb2
Repkit <ionut.baches@yahoo.com>
2020-01-24 10:33:34 +0200

    add misses also

--- models/cache.js
+++ models/cache.js
@@ -2,14 +2,49 @@ const sqlite3 = require('sqlite3').verbose()
 const file = __dirname + '/../data/db/images.db'
 const db = new sqlite3.Database(file)
 
- exports.getCacheHits = async (cb) =>  {
-    var hits = 0
-    await db.all("SELECT sum(hit) as hits FROM files", function(err, rows) {
-        rows.forEach(function (row) {
-            // console.log('row: ', row.hits)
-            cb(row.hits)
+exports.getCacheHits = (cb) =>  {
+    var hits, misses = 0
+    db.serialize(function() {
+        db.get("SELECT sum(hit) as hits, count(id) as misses FROM files", function(err, row) {
+            // console.log("hits : "+row.hits);
+            hits = row.hits
+            misses = row.misses
+            cb(hits, misses)
         })
-	})
-	   
-    db.close()
+    })
+    
+    // db.close()
+}
+
+exports.increaseCacheHits = async (image, size, format) =>  {
+    
+    await db.serialize(function() {
+        
+        let stmt = db.prepare("update files set hit = hit+1 where name = ? and extension = ? and size = ?")
+        
+        stmt.run(image, format, size)
+        
+        stmt.finalize()
+        
+    });
+    
+    // db.close();
+    
+}
+
+exports.insertCache = (image, size, format) =>  {
+    db.serialize(function() {
+        let data = [image, format, size] 
+        let sql = `insert into files (name, extension, size, hit) values (?, ?, ?, 0)`
+        db.run(sql, data, function(err) {
+            if (err) {
+                return console.error(err.message);
+            }
+            
+            console.log(`Row(s) updated: ${this.changes}`);
+        });
+    });
+    
+    // close the database connection
+    // db.close();
 }
